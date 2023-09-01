cab755f0ba2e7a39871a5ee534186cc1cd1f0f4f
AlejandroSZIOSA <ggato33@gmail.com>
2023-09-01 11:37:24 +0200

    Creating Dummy Data

---
+++ src/data/dummy-data.js
@@ -0,0 +1,9 @@
+import Product from "../models/product";
+//Creating Dummy Data
+export const PRODUCTS = [ 
+  new Product('product1','product2 Description',10,0),
+  new Product('product1','product2 Description',20,0),
+  new Product('product1','product2 Description',30,0),
+  new Product('product1','product2 Description',40,0),
+  new Product('product1','product2 Description',50,0),
+];
---
+++ src/models/product.js
@@ -0,0 +1,9 @@
+class Product{
+  constructor(name, description,price, buyAmount){
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.buyAmount = buyAmount;
+  }
+}
+export default Product;

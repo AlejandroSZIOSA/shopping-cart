1670c61991015ae03e1d4a744d1c91f31cbc4c94
AlejandroSZIOSA <ggato33@gmail.com>
2023-09-01 11:23:40 +0200

    some changes

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-// file structure
+// file structure 2
 export default function App() {
   return (
     <View style={styles.container}>

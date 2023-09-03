0dfb9a8f4447e9717fb7a825b21e08694b492c28
AlejandroSZIOSA <ggato33@gmail.com>
2023-09-03 18:03:14 +0200

    refactor

--- src/screens/ShoppingScreen.js
+++ src/screens/ShoppingScreen.js
@@ -1,6 +1,6 @@
 import { View,StyleSheet,Text, FlatList,SafeAreaView,Platform } from "react-native";
 import { PRODUCTS } from "../data/dummy-data";
-import ProductRowTile from "../components/ProductRowTile";
+import ProductRowTile from "../components/cardviews/ProductRowTile";
 
 //Function renders a product item and takes PRODUCTS data as a parameter :)
 function renderProductItem(itemData){

b9521dbddd9be1e91df380a6c84b0a3561ba8936
AlejandroSZIOSA <ggato33@gmail.com>
2023-09-03 09:54:20 +0200

    First working Dynamic List

---
+++ src/components/ProductRowTile.js
@@ -0,0 +1,12 @@
+import { View,Text,StyleSheet } from "react-native";
+
+function ProductRowTile({props} ) {
+  return(
+    <View>
+      <Text> props.name </Text>
+    </View>
+  );
+
+}
+export default ProductRowTile;
+
--- src/screens/ShoppingScreen.js
+++ src/screens/ShoppingScreen.js
@@ -1,9 +1,29 @@
-import { View,StyleSheet,Text } from "react-native";
+import { View,StyleSheet,Text, FlatList } from "react-native";
+import { PRODUCTS } from "../data/dummy-data";
+import ProductRowTile from "../components/ProductRowTile";
+
+//Function renders a product item
+function renderCategoryItem(itemData){
+  return (
+    <ProductRowTile 
+      title={itemData.item.name} 
+      //price={itemData.item.price}
+    />
+  )
+}
 
 function ShoppingScreen(){
   return(
     <View>
       <Text style={styles.text}> Shopping Screen </Text>
+        
+          <FlatList
+            data={PRODUCTS}
+            keyExtractor={(item) => { item.id}}
+            renderItem={renderCategoryItem}
+            //numColumns={2} // can change this :)
+          />
+        
     </View>
   );
 }

a99a8e21f5e30a7c3a649223672c1bff03b3c760
AlejandroSZIOSA <ggato33@gmail.com>
2023-09-17 18:40:26 +0200

    Fixing Dummy Data

--- src/data/dummy-data.js
+++ src/data/dummy-data.js
@@ -1,9 +1,9 @@
 import Product from "../models/product";
 //Creating Dummy Data
 export const PRODUCTS = [ 
-  new Product("product1",'Description1',10,0),
-  new Product("product2",'Description2',20,0),
-  new Product("product3",'Description3',30,0),
-  new Product("product4",'product2 Description',40,0),
-  new Product("product5",'product2 Description',50,0),
+  new Product("Product 1","Description 1",10,0),
+  new Product("Product 2","Description 2",20,0),
+  new Product("Product 3","Description 3",30,0),
+  new Product("Product 4","Description 4",40,0),
+  new Product("Product 5","Description 5",50,0),
 ];

c3ff1fe7ccec68fee7b489a5a81b2c49daceccb3
AlejandroSZIOSA <ggato33@gmail.com>
2023-10-14 10:06:30 +0200

    Styling Description screen

--- src/data/dummy-data.js
+++ src/data/dummy-data.js
@@ -1,9 +1,9 @@
 import Product from "../models/product";
 //Creating Dummy Data
 export const PRODUCTS = [ 
-  new Product(0,"Product 1","Description 1",10,0),
-  new Product(1,"Product 2","Description 2",20,0),
-  new Product(2,"Product 3","Description 3",30,0),
-  new Product(3,"Product 4","Description 4",40,0),
-  new Product(4,"Product 5","Description 5",50,0),
+  new Product(1,"Product 1","Description 1",10,0),
+  new Product(2,"Product 2","Description 2",20,0),
+  new Product(3,"Product 3","Description 3",30,0),
+  new Product(4,"Product 4","Description 4",40,0),
+  new Product(5,"Product 5","Description 5",50,0),
 ];
--- src/screens/DescriptionScreen.js
+++ src/screens/DescriptionScreen.js
@@ -1,14 +1,22 @@
-import { StyleSheet, View,Text } from "react-native";
+import { StyleSheet, View,Text,SafeAreaView } from "react-native";
 import { PRODUCTS } from "../data/dummy-data";
+import UI_COLORS from "../constants/uiColors";
 
 // Can use "route" and "navigation" params because is register in a navigation-stack
 function DescriptionScreen({route}){
   const productId = route.params.productId;
 
   return (
-    <View style={styles.container}> 
-      <Text> Description Screen - {productId} </Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.titleContainer}> 
+        <Text style={styles.titleText}> Description SC</Text>
+      </View>
+
+      <View style={styles.resultContainer}> 
+        <Text style={styles.resultText}> Object ID = {productId}</Text>
+      </View>
+
+    </SafeAreaView>
   )
 
 }
@@ -19,6 +27,30 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
     alignContent:'center',
-  }
+    justifyContent:'center',
+    backgroundColor: UI_COLORS.DESCRIPTION_BTN_BACKGROUND,
+  },
+
+  titleContainer:{
+    padding: 20,
+
+  },
+
+  titleText:{
+    textAlign: 'center',
+    fontSize: 40,
+  },
+
+  resultContainer:{
+    padding: 20,
+  },
+
+  resultText:{
+    textAlign: 'center',
+    fontSize: 40,
+  },
+
+
+
 
 });
